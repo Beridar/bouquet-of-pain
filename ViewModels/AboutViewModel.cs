d2011f7eb54bb8c10e7d0bd7b5548fbc012aa9bf
Robert Norell <robert.norell@outlook.com>
2019-01-24 20:28:47 -0600

    Tappable URLs now.

--- ViewModels/AboutViewModel.cs
+++ ViewModels/AboutViewModel.cs
@@ -1,10 +1,17 @@
-﻿namespace BouquetOfPain
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace BouquetOfPain
 {
     public class AboutViewModel : BaseViewModel
     {
         public AboutViewModel()
         {
             Title = "About";
+
+            OpenUrl = new Command<string>(url => Device.OpenUri(new System.Uri(url)));
         }
+
+        public ICommand OpenUrl { get; }
     }
 }
