bf470d336201377d9b9ca8ceaf3c7fbdd809f3fe
Robert Norell <robert.norell@outlook.com>
2019-01-28 19:30:14 -0600

    Attach a simple dieroller.

--- ViewModels/RollerViewModel.cs
+++ ViewModels/RollerViewModel.cs
@@ -1,6 +1,49 @@
-﻿namespace BouquetOfPain.ViewModels
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using BouquetOfPain.Models;
+using Xamarin.Forms;
+
+namespace BouquetOfPain.ViewModels
 {
     public class RollerViewModel : BaseViewModel
     {
+        public List<int> DiceOptions { get; }
+        public List<int> DiceSizes { get; }
+        public ICommand Roll { get; }
+
+        public int SelectedDice { get; set; }
+        public int SelectedSize { get; set; }
+
+        private string result;
+        public string Result
+        {
+            get => result;
+            private set
+            {
+                result = value;
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+
+        private readonly Roller randomNumberGenerator;
+
+        public RollerViewModel()
+        {
+            DiceOptions = Enumerable.Range(1, 100).ToList();
+            DiceSizes = new List<int>
+            {
+                2, 3, 4, 6, 8, 10, 12, 20, 24, 30, 100
+            };
+
+            Roll = new Command(ExecuteRoll);
+
+            randomNumberGenerator = new Roller();
+        }
+
+        private void ExecuteRoll()
+        {
+            Result = randomNumberGenerator.Roll(SelectedDice, SelectedSize).Total.ToString();
+        }
     }
 }
