c2cc3eb7d1d24a07a6d58b693151059c6921f4cb
Robert Norell <robert.norell@outlook.com>
2019-01-29 21:19:02 -0600

    We can shade the background of this based on how much of it is filled.

--- Controls/PercentageWithShader.xaml.cs
+++ Controls/PercentageWithShader.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System.ComponentModel;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace BouquetOfPain.Controls
@@ -13,13 +14,77 @@ namespace BouquetOfPain.Controls
 
         public string Text
         {
-            get => GetValue(TextProperty) as string;
+            get => (string)GetValue(TextProperty);
             set => SetValue(TextProperty, value);
         }
+
+        public static BindableProperty ShaderBackgroundColorProperty = BindableProperty.Create(
+            nameof(ShaderBackgroundColor),
+            typeof(Color),
+            typeof(PercentageWithShader));
+
+        public Color ShaderBackgroundColor
+        {
+            get => (Color) GetValue(ShaderBackgroundColorProperty);
+            set => SetValue(ShaderBackgroundColorProperty, value);
+        }
+
+        public static BindableProperty ShaderLayoutBoundsProperty = BindableProperty.Create(
+            nameof(ShaderLayoutBounds),
+            typeof(Rectangle),
+            typeof(PercentageWithShader));
+
+        public Rectangle ShaderLayoutBounds
+        {
+            get => (Rectangle)GetValue(ShaderLayoutBoundsProperty);
+            set => SetValue(ShaderLayoutBoundsProperty, value);
+        }
+
+        public static BindableProperty PercentageProperty = BindableProperty.Create(
+            nameof(Percentage),
+            typeof(double),
+            typeof(PercentageWithShader));
+
+        public double Percentage
+        {
+            get => (double)GetValue(PercentageProperty);
+            set => SetValue(PercentageProperty, value);
+        }
+
+        public const double MINIMUM_PERCENTAGE_TO_SHOW = 0.048;
+        public const double MAXIMUM_PERCENTAGE_TO_SHOW = 0.052;
 
         public PercentageWithShader()
         {
             InitializeComponent();
+
+            PropertyChanged += UpdateShadingWhenPercentageChanges;
+        }
+
+        private void UpdateShadingWhenPercentageChanges(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName != nameof(Percentage)) return;
+
+            if (Percentage < MINIMUM_PERCENTAGE_TO_SHOW)
+            {
+                ShaderLayoutBounds = new Rectangle(0, 0, 0, 0);
+                ShaderBackgroundColor = Color.Transparent;
+            }
+
+            else if (Percentage > MAXIMUM_PERCENTAGE_TO_SHOW)
+            {
+                ShaderLayoutBounds = new Rectangle(0, 0, 1, 1);
+                ShaderBackgroundColor = Color.Red;
+            }
+
+            else
+            {
+                var percentageToFill = (Percentage - MINIMUM_PERCENTAGE_TO_SHOW) /
+                                       (MAXIMUM_PERCENTAGE_TO_SHOW - MINIMUM_PERCENTAGE_TO_SHOW);
+
+                ShaderLayoutBounds = new Rectangle(0, 0, percentageToFill, 1);
+                ShaderBackgroundColor = Color.Green;
+            }
         }
     }
 }
