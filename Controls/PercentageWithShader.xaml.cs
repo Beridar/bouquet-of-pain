53b370362836107c36d084499ddf63212269f017
Robert Norell <robert.norell@outlook.com>
2019-01-29 21:08:43 -0600

    This deserves to be its own control

---
+++ Controls/PercentageWithShader.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BouquetOfPain.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PercentageWithShader : ContentView
+    {
+        public static BindableProperty TextProperty = BindableProperty.Create(
+            nameof(Text),
+            typeof(string),
+            typeof(PercentageWithShader));
+
+        public string Text
+        {
+            get => GetValue(TextProperty) as string;
+            set => SetValue(TextProperty, value);
+        }
+
+        public PercentageWithShader()
+        {
+            InitializeComponent();
+        }
+    }
+}
